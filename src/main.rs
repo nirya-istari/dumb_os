35ca47b18563813be6120bb28c08a14b4203799c
Triss Healy <trissylegs@gmail.com>
2021-02-27 17:24:49 +1100

    Boots from bootload

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,18 @@
 
 mod panic;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
